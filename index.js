7fde3ca848ccadfc9408dbc0ba01cfcda496960b
MarkXtenda <mark.i2p.1998@gmail.com>
2022-06-17 19:36:16 +1000

    Final

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code here
+class Dinner {
+    constructor(salad, soup, entree, _dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = _dessert;
+    }
+} 
+
+class Breakfast {
+    constructor(food, drink){
+        this.food = food
+        this.drink = drink
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+
+let newdinner = new Dinner('balsamic salad', 'consomme', 'filet mignon', 'cheesecake')
+
+let newlunch = new Lunch('side salad', 'broccoli cheddar soup', 'iced tea')
+
+let bfast = new Breakfast('eggs', 'juice')
